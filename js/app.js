dce6b6e6d7441e6ef836ebd738b2e8a3e7e84612
Sky Ta <tat491@newschool.edu>
2021-04-22 23:02:59 -0400

    Procfile created

--- js/app.js
+++ js/app.js
@@ -165,4 +165,12 @@ function createDownloadLink(blob) {
 
 	//add the li element to the ol
 	recordingsList.appendChild(li);
+
+	app.listen(process.env.PORT);
+
+	let port = process.env.PORT;
+		if (port == null || port == "") {
+  port = 8000;
+}
+		app.listen(port);
 }
